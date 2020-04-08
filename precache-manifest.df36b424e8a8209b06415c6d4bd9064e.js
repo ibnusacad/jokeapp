88db06322d264ff232893eac77ea48528cfd5c79
abdiqayum <isacad1920@gmail.com>
2020-04-09 02:28:42 +0300

    Updates

---
+++ precache-manifest.df36b424e8a8209b06415c6d4bd9064e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47421461ec17aa9b92539357e414f7e7",
+    "url": "/jokeapp/index.html"
+  },
+  {
+    "revision": "300734c20e89b4733a5a",
+    "url": "/jokeapp/static/css/2.20e5802c.chunk.css"
+  },
+  {
+    "revision": "5e0265c9d3ee0f808de4",
+    "url": "/jokeapp/static/css/main.de7a7aed.chunk.css"
+  },
+  {
+    "revision": "300734c20e89b4733a5a",
+    "url": "/jokeapp/static/js/2.15595e9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jokeapp/static/js/2.15595e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e0265c9d3ee0f808de4",
+    "url": "/jokeapp/static/js/main.61d26528.chunk.js"
+  },
+  {
+    "revision": "93a072e6ebad6ee4a523",
+    "url": "/jokeapp/static/js/runtime-main.96100d20.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/jokeapp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/jokeapp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/jokeapp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/jokeapp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/jokeapp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
